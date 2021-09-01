230825b0fec2eb590bf0a149a3a91b6e4a11ab3d
Yesenia L <ylopezcr123@gmail.com>
2021-09-01 18:21:53 -0400

    home route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
-//add home page
-const App = () => {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+// import Login from "./Pages/Login";
+// import FourOFour from "./Pages/FourOFour";
+import Home from "./Pages/Home";
+// import Index from "./Pages/Index";
+// import Questionaire from "./Pages/Questionare";
+// import Show from "./Pages/Show";
+//import Navbar from "./Components/Navbar"
+
+function App(){
   return (
     <div>
-    {/* Home path */}
-      
+      <Router>
+        {/* <Navbar /> */}
+        <main>
+          <Switch>
+            <Route exact path="/" >
+              <Home />
+            </Route>
+            
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 };
 
 export default App;
+//routes
+// INDEX/login
+// INDEX/signup
+// SHOW /users/:id
+// UPDATE /users/:id/edit
+// CREATE /users/new
+// SHOW/relevant
+// SHOW/users/:id/matches/
+
+
+// <Route exact path="/demo">
+// <Index/>
+// </Route>
+// <Route>
+// <Login/>
+// </Route>
+// <Route>
+// <FourOFour/>
+// </Route>
+// <Route>
+// <Questionaire/>
+// </Route>
+// <Route>
+// <Show/>
+// </Route>
