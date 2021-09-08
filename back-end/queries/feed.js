e0a4a9e76b040fcc452255b184393eeab2f8814d
Talia Pichardo <taliapichardo@gmail.com>
2021-09-08 15:55:49 -0400

    Adds incoming changes

--- back-end/queries/feed.js
+++ back-end/queries/feed.js
@@ -1,13 +1,24 @@
 const db = require('../database/dbConfig');
 
 const getAllPosibleMatches = async (id) => {
+  const { id } = req.params
   try {
-  
-} catch (err) {
-  return 'error'
-}
+
+  } catch (err) {
+    return 'error'
+  }
 };
 
 moduel.exports = {
-getAllPosibleMatches,
+  getAllPosibleMatches,
 }
+/* INDEX */
+feed.get("/", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const allMatches = await getAllPosibleMatches(userId);
+    res.status(200).json(allMatches);
+  } catch (err) {
+    res.status(404).statusMessage(err);
+  }
+});
