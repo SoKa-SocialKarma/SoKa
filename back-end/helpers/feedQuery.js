5ae22a533dc0953d60609e87cb5d95d33464d3ff
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-09-10 13:08:43 -0400

    Queries passing 98%, two bugs encounter

--- back-end/helpers/feedQuery.js
+++ back-end/helpers/feedQuery.js
@@ -1,19 +1,56 @@
-const users = require("../controllers/usersController")
+const getAllPossibleMatchesQuery = ({ id, radius }) => {
+  if (radius) {
+    return `SELECT id, username, radius FROM users WHERE id != ${id} AND radius <= ${radius};`
+  }
 
-const getAllPossibleMatchesQuery = ({id, goals, radius, matches, availability})=>{
-
-    let qString = 
-`WITH RECURSIVE matches (id, username, goalsList) AS (SELECT id, username, goals->'goals' FROM users WHERE id = ${id} UNION ALL SELECT users.id, users.username, users.goals FROM users JOIN matches ON users.goals->'goals' @> matches.goalsList) SELECT DISTINCT * FROM matches WHERE id != ${id};`
-    const qParams = [];
-
-
-    // if (goals) {
-    //     qParams.push(`%${location}%`);
-    //     qString += `${qString.includes("WHERE") ? " AND" : " WHERE"} location ILIKE $${qParams.length}`;
-    // }
-
-
-    return { qString, qParams };
+  return `WITH RECURSIVE matches (id, username, goalsList) \n
+  AS (SELECT id, username, goals->'goals' FROM users WHERE id = ${id} \n
+  UNION ALL SELECT users.id, users.username, users.goals FROM users \n
+  JOIN matches ON users.goals->'goals' @> matches.goalsList) \n
+  SELECT DISTINCT * FROM matches WHERE id != ${id};`
 }
 
-module.exports = {getAllPossibleMatchesQuery}
+const getFilteredGoalsQuery = id => {
+  return `WITH RECURSIVE matches (id, username, goalsList) \n
+    AS (SELECT id, username, goals->'goals' FROM users WHERE id = ${id} \n
+    UNION ALL SELECT users.id, users.username, users.goals FROM users \n
+    JOIN matches ON users.goals->'goals' @> matches.goalsList) \n
+    SELECT DISTINCT * FROM matches WHERE id != ${id};`
+}
+
+const getFilteredRadiusQuery = id => {
+  return `WITH RECURSIVE matches (miles, id, username, radius) \n
+    AS (SELECT radius, id, username, radius FROM users WHERE id = ${id} \n
+    UNION SELECT users.radius, users.id, users.username, users.radius FROM users \n
+    INNER JOIN matches ON matches.miles >= users.radius) SELECT id, username, radius FROM matches WHERE id !=${id};`
+}
+
+const getFilteredMatchesQuery = id => {
+
+    return `SELECT matchRequests->'matchRequests' as matchesList FROM users WHERE id = ${id}`
+
+    // return `WITH RECURSIVE matches (object, id, username, list) \n
+    // AS (SELECT matchRequests, id, username, CAST (matchRequests->'matchRequests' AS INTEGER) FROM users WHERE id = ${id} \n
+    // UNION SELECT users.matchRequests, CAST( users.matchRequests->'matchRequests' AS INTEGER), users.username, users.id FROM users \n
+    // INNER JOIN matches ON matches.list = users.id) SELECT DISTINCT * FROM matches WHERE id != ${id};`
+}
+
+// WITH RECURSIVE matches (object, id, username, list) AS (SELECT matchRequests, id, username, CAST (matchRequests->'matchRequests' AS INTEGER) FROM users WHERE id = 1 UNION SELECT users.matchRequests, CAST( users.matchRequests->'matchRequests' AS INTEGER), users.username, users.id FROM users INNER JOIN matches ON matches.list = users.id) SELECT * FROM matches;
+
+const getFilteredAvailabilityQuery = id => {
+    
+    return `SELECT availability->'days' as daysList FROM users WHERE id = ${id}` 
+}
+
+
+
+
+
+
+module.exports = {
+  getAllPossibleMatchesQuery,
+  getFilteredGoalsQuery,
+  getFilteredRadiusQuery,
+  getFilteredMatchesQuery,
+  getFilteredAvailabilityQuery
+}
--- back-end/queries/feed.js
+++ back-end/queries/feed.js
@@ -1,52 +1,80 @@
-const db = require('../database/dbConfig');
+const db = require('../database/dbConfig')
 
-const { getAllPossibleMatchesQuery} = require('../helpers/feedQuery')
+const {
+  getAllPossibleMatchesQuery,
+  getFilteredGoalsQuery,
+  getFilteredRadiusQuery,
+  getFilteredMatchesQuery,
+  getFilteredAvailabilityQuery
+} = require('../helpers/feedQuery')
 
-const getAllPossibleMatches = async (id) => {
+const getAllPossibleMatches = async sokaQuery => {
   try {
-    const dbFeedQuery = getAllPossibleMatchesQuery(id)
-    // if (!dbFeedQuery.qParams.length){
-    // }
+    return await db.any(getAllPossibleMatchesQuery(sokaQuery))
+  } catch (err) {
+    return console.error(err.message)
+  }
+}
 
-    const possibleMatches = await db.any(dbFeedQuery.qString)
-    return possibleMatches
-} catch (err) {
-  return 'Error query not returned'
-}
-};
-const getFilteredGoals = async (id) => {
+const getFilteredGoals = async id => {
   try {
-  
-} catch (err) {
-  return 'error'
+    return await db.any(getFilteredGoalsQuery(id))
+  } catch (err) {
+    return console.error(err.message)
+  }
 }
-};
-const getFilteredRadius = async (id) => {
+
+const getFilteredRadius = async id => {
   try {
-  
-} catch (err) {
-  return 'error'
+    return await db.any(getFilteredRadiusQuery(id))
+  } catch (err) {
+    return console.error(err.message)
+  }
 }
-};
-const getFilteredMatches = async (id) => {
+
+const getFilteredMatches = async id => {
   try {
-  
-} catch (err) {
-  return 'error'
+    const matchesList =
+      '' + Object.values(await db.one(getFilteredMatchesQuery(id)))
+    if (!matchesList.includes(',')) {
+      return await db.one('SELECT * FROM users WHERE id=$1', matchesList)
+    }
+    return await db.tx(t => {
+      const queries = matchesList
+        .split(',')
+        .map(id => t.one('SELECT * FROM users WHERE id=$1', id))
+      return t.batch(queries)
+    })
+  } catch (err) {
+    return console.error(err.message)
+  }
 }
-};
-const getFilteredAvailability = async (id) => {
+
+const getFilteredAvailability = async id => {
   try {
-  
-} catch (err) {
-  return 'error'
+
+    const daysList ='' + Object.values(await db.one(getFilteredAvailabilityQuery(id)))
+  console.log(daysList)
+    if (!daysList.includes(',')) {
+      return await db.many(`SELECT id, username, availability FROM users WHERE jsonb_path_exists(availability, '$.days[*] ? (@ == "${daysList}")') AND id != ${id}`, daysList)
+    }
+
+    return await db.tx(t => {
+      const queries = daysList
+        .split(',')
+        .map(day =>  t.many(`SELECT id, username, availability FROM users WHERE jsonb_path_exists(availability, '$.days[*] ? (@ == "${day}")') AND id != ${id}`, day))
+      return t.batch(queries)
+    })
+
+  } catch (err) {
+    return console.error(err.message)
+  }
 }
-};
 
 module.exports = {
-getAllPossibleMatches,
-getFilteredGoals,
-getFilteredRadius,
-getFilteredMatches,
-getFilteredAvailability
+  getAllPossibleMatches,
+  getFilteredGoals,
+  getFilteredRadius,
+  getFilteredMatches,
+  getFilteredAvailability
 }
