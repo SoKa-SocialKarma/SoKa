2be2f7685c0c7ff385055ac27fa7c4f2ac3128db
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-09-12 02:14:16 -0400

    Queries working 100%

--- back-end/controllers/feedController.js
+++ back-end/controllers/feedController.js
@@ -1,6 +1,48 @@
+/**  =====================================================  
+*                 filteredNestedDuplicates
+*    =====================================================
+ 
+*   Takes in an array of arrays and flattens it into a 
+*   single array of non duplicated Objects.
+
+*   @param   {Array} list - An array of arrays.  
+*   @returns {Array} data - An array of Objects.
+*   [[{2},{3},{5}],[{1},{2}],[{5}]] ->>> [{1},{2},{3},{5}]
+**/
+
+
+const filteredNestedDuplicates = list => {
+  if (list === undefined) {
+    return 'No data found with the current id.!'
+  }
+
+  let data = {}
+  let flat = []
+
+  if (list.length === 1) {
+    return list
+  }
+
+  list.forEach(arr => {
+    flat = [...flat, ...arr]
+  })
+  flat.forEach(item => {
+    if (!data[item.id]) {
+      data[item.id] = item
+    }
+  })
+  return Object.values(data)
+}
+
+/**  =====================================================  
+*                     feedController
+*    =====================================================
+**/
+
 const feed = require('express').Router({ mergeParams: true })
 const {
   getAllPossibleMatches,
+  getFilteredFriends,
   getFilteredGoals,
   getFilteredRadius,
   getFilteredMatches,
@@ -15,7 +57,18 @@ feed.get('/', async (req, res) => {
     const allMatches = await getAllPossibleMatches(sokaQuery)
     res.status(200).json(allMatches)
   } catch (err) {
-    res.status(404).send('Error')
+    res.status(404).send(err)
+  }
+})
+
+feed.get('/friends', async (req, res) => {
+  const { id } = req.params
+  try {
+    const filteredFriends = await getFilteredFriends(id)
+    const data = await filteredNestedDuplicates(filteredFriends)
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(404).send(err)
   }
 })
 
@@ -23,9 +76,10 @@ feed.get('/goals', async (req, res) => {
   const { id } = req.params
   try {
     const filteredGoals = await getFilteredGoals(id)
-    res.status(200).json(filteredGoals)
+    const data = await filteredNestedDuplicates(filteredGoals)
+    res.status(200).json(data)
   } catch (err) {
-    res.status(404).statusMessage(err)
+    res.status(404).send(err)
   }
 })
 
@@ -35,7 +89,7 @@ feed.get('/radius', async (req, res) => {
     const filteredRadius = await getFilteredRadius(id)
     res.status(200).json(filteredRadius)
   } catch (err) {
-    res.status(404).statusMessage(err)
+    res.status(404).send(err)
   }
 })
 
@@ -45,7 +99,7 @@ feed.get('/matches', async (req, res) => {
     const filteredMatches = await getFilteredMatches(id)
     res.status(200).json(filteredMatches)
   } catch (err) {
-    res.status(404).statusMessage(err)
+    res.status(404).send(err)
   }
 })
 
@@ -56,31 +110,8 @@ feed.get('/availability', async (req, res) => {
     const data = await filteredNestedDuplicates(filteredAvailability)
     res.status(200).json(data)
   } catch (err) {
-    res.status(404).statusMessage(err)
+    res.status(404).send(err)
   }
 })
-
-
-// filteredNestedDuplicates
-// [[{2},{3},{5}],[{1},{2}],[{5}]] ->>> [{1},{2},{3},{5}]
-
-const filteredNestedDuplicates = list => {
-  let data = {}
-  let flat = []
-
-  if(list.length ===1) {
-    return list
-  }
-  
-  list.forEach(arr => {
-    flat = [...flat, ...arr]
-  })
-  flat.forEach(item => {
-    if (!data[item.id]) {
-      data[item.id] = item
-    }
-  })
-  return Object.values(data)
-}
 
 module.exports = feed
---
+++ back-end/helpers/usersQuery.js
@@ -0,0 +1,168 @@
+/**  =====================================================================  
+*                            getAllUsersQuery
+*    =====================================================================
+* 
+*    Takes in an Object with the following key/value pairs:
+*
+*    {id,name,lastname,username,location,gender,radius,karma,
+*      badges, goal, experience, availability, sortBy, order}
+
+*    @param   {Object} sokaQuery - An Object from req.query.  
+*    @returns {String} qString - A database query for PSQL.
+*    @returns {String} qString - If no argument is passed, returns a query 
+*                      with all the users in the database.
+*
+*    @example sokaQuery = 
+*    'http://localhost:4000/users?location=Glendale&name=Carlos
+*     &lastname=Cardozo&username=EZ&radius=7&karma=5&goal=Cardio
+*     &experience=Body'
+*   
+*    qString, will verify each argument of the request query, and
+*    proceed to create a database query that will return the desired search.
+*   
+*    @summary Will return a database query for PSQL. 
+*    Limited to 1 goal or 1 experience or 1 availability, at a time.
+*    Able to sort by argument in ascendent or descendent order with
+*    the sortBy & order parameters.
+**/
+
+
+
+const getAllUsersQuery = ({
+  name,
+  lastname,
+  username,
+  location,
+  gender,
+  radius,
+  karma,
+  badges,
+  goal,
+  experience,
+  availability,
+  sortBy,
+  order
+}) => {
+  let qString = ''
+
+  if (goal) {
+    qString = `SELECT * FROM users WHERE EXISTS (SELECT * FROM \n
+    jsonb_array_elements_Text(goals->'goals') as g(tag) WHERE g.tag ILIKE '%${goal}%') `
+  } else if (experience) {
+    qString = `SELECT * FROM users WHERE EXISTS (SELECT * FROM \n
+    jsonb_array_elements_Text(experience->'experience') as e(tag) WHERE e.tag ILIKE '%${experience}%') `
+  } else if (availability) {
+    qString = `SELECT * FROM users WHERE EXISTS (SELECT * FROM \n
+    jsonb_array_elements_Text(availability->'days') as d(tag) WHERE d.tag ILIKE '%${availability}%') `
+  } else {
+    qString = `SELECT * FROM users `
+  }
+
+  if (name) {
+    qString += ` WHERE name ILIKE '%${name}%'`
+  }
+  if (lastname) {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } lastname ILIKE '%${lastname}%'`
+  }
+  if (username) {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } username ILIKE '%${username}%'`
+  }
+  if (location) {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } location ILIKE '%${location}%'`
+  }
+  if (gender) {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } gender = '${gender}'`
+  }
+  if (radius) {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } radius = '${radius}'`
+  }
+  if (karma) {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } karma <= ${karma}`
+  }
+
+  badges = badges?.toUpperCase()
+  if (badges === 'TRUE' || badges === 'FALSE') {
+    qString += `${
+      qString.includes('WHERE') ? ' AND' : ' WHERE'
+    } badges=${badges}`
+  }
+
+  const columns = [
+    'id',
+    'name',
+    'lastname',
+    'username',
+    'location',
+    'gender',
+    'radius',
+    'karma'
+  ]
+  sortBy = sortBy?.toLowerCase()
+  order = order?.toUpperCase()
+  if (columns.includes(sortBy)) {
+    qString += ` ORDER BY ${sortBy} ${
+      order === 'ASC' || order === 'DESC' ? order : 'ASC'
+    }`
+  }
+
+  return qString
+}
+
+const updateUsersQuery = (ids, data) => {
+  const constructQuery = (id, user) => {
+    let qString = 'UPDATE users SET'
+    const qParams = []
+
+    const params = [
+      'name',
+      'lastname',
+      'username',
+      'location',
+      'gender',
+      'radius',
+      'karma',
+      'image',
+      'goals',
+      'experience',
+      'availability',
+      'matchRequests',
+      'pendingReview'
+    ]
+    for (const key in user) {
+      if (params.includes(key)) {
+        qParams.push(user[key])
+        qString += `${qParams.length > 1 ? ',' : ''} ${key}=$${qParams.length}`
+      }
+    }
+
+    qParams.push(id)
+    qString += ` WHERE id=$${qParams.length} RETURNING *`
+    return { qString, qParams }
+  }
+
+  if (!ids.includes(',')) {
+    if (data.length) {
+      data = data[0]
+    }
+    return constructQuery(ids, data)
+  }
+
+  return ids.split(',').map((id, i) => constructQuery(id, data[i]))
+}
+
+module.exports = {
+  getAllUsersQuery,
+  updateUsersQuery
+}
