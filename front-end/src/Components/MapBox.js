fb9ed33f4f5c1a551046315d17426750118f31ac
Talia Pichardo <taliapichardo@gmail.com>
2021-09-14 01:06:58 -0400

    Imports ReactMapGL

---
+++ front-end/src/Components/MapBox.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MapBox = () => {
+    return (
+        <div>
+            <h1>Mappppp</h1>
+        </div>
+    )
+}
+
+export default MapBox;
