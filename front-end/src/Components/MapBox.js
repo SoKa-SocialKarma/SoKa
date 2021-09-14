c003ac827bb38adb223eac4c02edeab58ccea9ec
Talia Pichardo <taliapichardo@gmail.com>
2021-09-14 01:08:43 -0400

    Adds state to mapbox component

--- front-end/src/Components/MapBox.js
+++ front-end/src/Components/MapBox.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import useGeoLocation from '../Hooks/useGeoLocation';
 
 const MapBox = () => {
+    const location = useGeoLocation();
+    const [viewport, setviewport] = useState({
+        latitude: location.coordinates.latitude || 40.7128,
+        longitude: location.coordinates.longitude || -74.0060,
+        zoom: 13,
+        width: window.innerWidth,
+        height: window.innerHeight
+    })
     return (
         <div>
             <h1>Mappppp</h1>
