39ca50304002521e711871d61d1507bafbb1d31c
Talia Pichardo <taliapichardo@gmail.com>
2021-09-14 17:20:30 -0400

    Adds SignUp.js

--- front-end/src/Pages/Signup.js
+++ front-end/src/Pages/Signup.js
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react';
+import { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../Context/AuthContext';
 import { Link, useHistory } from 'react-router-dom'
@@ -9,25 +9,25 @@ const SignUp = () => {
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
     const { signUp } = useAuth();
-    const [ error, setError ] = useState('');
-    const [ loading, setLoading ] = useState(false);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
     const history = useHistory();
 
     async function handleSubmit(e) {
         e.preventDefault();
 
-        if(passwordRef.current.value !== passwordConfirmRef.current.value) {
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
             return setError('Passwords do not match!');
         }
 
-        try{
+        try {
             setError('');
             setLoading(true);
             await signUp(emailRef.current.value, passwordRef.current.value);
             history.push('/');
-        }catch(error){
+        } catch (error) {
             const message = error.message.split(" ")
-            .filter((word)=> { return word !== "Firebase:" && word !=="(auth/weak-password)."}).join(" ")
+                .filter((word) => { return word !== "Firebase:" && word !== "(auth/weak-password)." }).join(" ")
             setError(`Failed to create an account. ${message}`);
         }
         setLoading(false);
@@ -35,30 +35,30 @@ const SignUp = () => {
 
     return (
         <>
-        <Card>
-            <Card.Body>
-                <h2 className="text-center mb-4" >Sign Up</h2>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} placeholder="example@soka.com" required/>
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} placeholder="Must have at least 6 characters" required/>
-                    </Form.Group>
-                    <Form.Group id="password-confirm">
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmRef} required/>
-                    </Form.Group>
-                    <Button disabled={loading} className="w-100 mt-4" type="submit">Sign Up</Button>
-                </Form>
-            </Card.Body>
-        </Card>            
-        <div className="w100 text-center mt-2">
-            Already have an account? <Link to="/login">Log In</Link>
-        </div>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4" >Sign Up</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} placeholder="example@soka.com" required />
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} placeholder="Must have at least 6 characters" required />
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} required />
+                        </Form.Group>
+                        <Button disabled={loading} className="w-100 mt-4" type="submit">Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w100 text-center mt-2">
+                Already have an account? <Link to="/login">Log In</Link>
+            </div>
         </>
     );
 };
