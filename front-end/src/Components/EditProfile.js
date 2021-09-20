4595323467a3f8e66adc824f667aa51fa211f388
Yesenia L <ylopezcr123@gmail.com>
2021-09-20 10:13:05 -0400

    nav changes

--- front-end/src/Components/EditProfile.js
+++ front-end/src/Components/EditProfile.js
@@ -38,14 +38,14 @@ export default function EditProfile () {
     event.preventDefault()
   }
   return (
-    <div id="edit">
-      {/* <h3>Edit page</h3> */}
-      {/* //avoid refreshing page  */}
-      <form onSubmit={handleSubmit} id="form">
-        <br />
+    <div id="form">
+      <form onSubmit={handleSubmit} >
+        <h4>Edit</h4>
         <span>
-          <h1>Jack Green</h1>
+          <label htmlFor="username">Username:</label>
+          <input type="text" />
         </span>
+        <br />
         <span>
         <label htmlFor="name">Name:</label>
         <input type="text" placeholder="name" />
@@ -74,35 +74,42 @@ export default function EditProfile () {
         </span>
         <br />
         <span>
-          <label htmlFor='experience'>Experience:</label>
-          <input
+          <label htmlFor="gender">Gender:</label>
+        <select name="" id="">
+          <option value="female">Female</option>
+          <option value="male">male</option>
+          <option value="trans">Trans</option>
+          </select>     
+          </span>
+   
+        <br />
+       
+          {/* <input
             type='text'
             id='experience'
             value={details.experience}
             onChange={handleChange}
-          />
-        </span>
-        <br />
+          /> */}
+        {/* </span> */}
+     
         <span>
           <label htmlFor='goals'>Goals:</label>
-          <input
+          <select name="goals" id="">
+          <option value="abs">Abs</option>
+          <option value="chest">Chest</option>
+          <option value="cardio">Cardio</option>
+          <option value="back">Back</option>
+          <option value="legs">Legs</option>
+          </select>
+          {/* <input
             type='text'
             id='goals'
             value={details.goals}
             onChange={handleChange}
-          />
+          /> */}
         </span>
           <br />
-        <span>
-          <label htmlFor='activity'>Activity:</label>
-          <input
-            type='text'
-            id='activity'
-            value={details.activity}
-            onChange={handleChange}
-          />
-        </span>
-        <br />
+          <br />
         <span>
         <button type='submit'>Submit</button>
         </span>
