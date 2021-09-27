df05999a043b9ba904e13a2ad1aab8a7c3f462bb
Talia Pichardo <taliapichardo@gmail.com>
2021-09-27 12:11:37 -0400

    removes code for a cleaner look

--- front-end/src/Components/Profile.js
+++ front-end/src/Components/Profile.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { apiURL } from "../Util/apiURL.js";
 // import { Link } from 'react-router-dom';
 import MapBox from "./MapBox";
+import purpBackground from '../Assets/purpBackground.jpg'
 
 // const API = apiURL();
 
@@ -29,95 +30,108 @@ function Profile() {
   } = currentUserData
 
   return (
-    <div class="container">
-
-      <div id="pro" class="item1">
-        <br />
-        <h2>About</h2>
-        <h6>
-          {" "}
-          Name: {name} {lastname}
-        </h6>
-        <h6>Gender: {gender}</h6>
-        <h6>Location:{location}</h6>
-        <h6>
-          Availablility:{availabledays[0]},{availabledays[1]}
-        </h6>
-        <h6>
-          Experience : {experience}
-        </h6>
-        <h6>
-          Goals: {goals[0]},{goals[1]}, {goals[2]}, {goals[3]}
-        </h6>
-        <h6>Radius:{radius} miles</h6>
-
+    <div>
+      <div id='profHeader'>
+        <img class='profBackground' src={purpBackground} alt='Two people working out' />
+        <img class='profPic' src="https://images.pexels.com/photos/2078265/pexels-photo-2078265.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" style={{ width: "200px", height: "180px" }} />
       </div>
+      <div id='card'>
+        <div id="prof">
+          <h2>{name} {lastname}</h2>
+          <br />
+          <br />
+        </div>
+      </div >
+      <div class="container">
+
+        <div id="pro" class="item1">
+          <br />
+          <h2>About</h2>
+          <h6>
+            {" "}
+            Name: {name} {lastname}
+          </h6>
+          <h6>Gender: {gender}</h6>
+          <h6>Location:{location}</h6>
+          <h6>
+            Availablility:{availabledays[0]},{availabledays[1]}
+          </h6>
+          <h6>
+            Experience : {experience}
+          </h6>
+          <h6>
+            Goals: {goals[0]},{goals[1]}, {goals[2]}, {goals[3]}
+          </h6>
+          <h6>Radius:{radius} miles</h6>
+
+        </div>
 
 
-      <div id="badges" class="item2">
-        <h2>Badges</h2>
-        <span>
-          <img src="https://cdn-icons-png.flaticon.com/512/2928/2928144.png" alt="" style={{ width: "40px" }} />
-          <>
-            <h4>Great Motivator</h4>
-            {/* <h6>-Enthusiastic</h6> */}
-          </>
-        </span>
+        <div id="badges" class="item2">
+          <h2>Badges</h2>
+          <span>
+            <img src="https://cdn-icons-png.flaticon.com/512/2928/2928144.png" alt="" style={{ width: "40px" }} />
+            <>
+              <h4>Great Motivator</h4>
+              {/* <h6>-Enthusiastic</h6> */}
+            </>
+          </span>
 
-        <span>
-          <img src="https://cdn-icons-png.flaticon.com/512/4053/4053735.png" alt="" style={{ width: "40px" }} />
-          <h4>Spot On</h4>
-          {/* <h6>-Assisted in lifting heavy weights safely</h6> */}
-        </span>
+          <span>
+            <img src="https://cdn-icons-png.flaticon.com/512/4053/4053735.png" alt="" style={{ width: "40px" }} />
+            <h4>Spot On</h4>
+            {/* <h6>-Assisted in lifting heavy weights safely</h6> */}
+          </span>
 
-        <span>
-          <img src="https://cdn-icons-png.flaticon.com/512/2843/2843974.png" alt="" style={{ width: "40px" }} />
-          <h4>Mobility Master</h4>
-          {/* <h6>-Stretch tightented muscles before workout</h6> */}
-        </span>
+          <span>
+            <img src="https://cdn-icons-png.flaticon.com/512/2843/2843974.png" alt="" style={{ width: "40px" }} />
+            <h4>Mobility Master</h4>
+            {/* <h6>-Stretch tightented muscles before workout</h6> */}
+          </span>
 
-        <span>
-          <img src="https://cdn-icons-png.flaticon.com/512/2090/2090622.png" alt="" style={{ width: "40px" }} />
-          <h4>Punctuality</h4>
-          {/* <h6>-Responds on time</h6> */}
-        </span>
+          <span>
+            <img src="https://cdn-icons-png.flaticon.com/512/2090/2090622.png" alt="" style={{ width: "40px" }} />
+            <h4>Punctuality</h4>
+            {/* <h6>-Responds on time</h6> */}
+          </span>
 
-        <span>
-          <img src="https://cdn-icons-png.flaticon.com/512/2237/2237680.png" alt="" style={{ width: "40px" }} />
-          <h4>Cardiologist</h4>
-          {/* <h6>-Increase BPM</h6> */}
-        </span>
-      </div>
-      <div id="socials" class="item3" >
+          <span>
+            <img src="https://cdn-icons-png.flaticon.com/512/2237/2237680.png" alt="" style={{ width: "40px" }} />
+            <h4>Cardiologist</h4>
+            {/* <h6>-Increase BPM</h6> */}
+          </span>
+        </div>
+        <div id="socials" class="item3" >
 
-        {/* <Link to={`/users/${index}/edit`}>
+          {/* <Link to={`/users/${index}/edit`}>
           <button type="button"  >
-            EDIT
-
+          EDIT
+          
           </button>
-        </Link>
-        <button>
+          </Link>
+          <button>
           NOTIFICATIONS
         </button> */}
 
 
-        <a href="https://www.instagram.com/" target="_blank">
-          <img src="https://cdn-icons-png.flaticon.com/128/1400/1400487.png" alt="ig" style={{ width: "50px" }} />
-        </a>
-        <a href="https://www.facebook.com/" target="_blank">
-          <img src="https://cdn-icons-png.flaticon.com/128/1400/1400477.png" alt="ig" style={{ width: "50px" }} />
-        </a>
-        <a href="https://web.whatsapp.com/">
-          <img src="https://cdn-icons-png.flaticon.com/128/3820/3820293.png" alt="ig" style={{ width: "50px" }} />
-        </a>
-        <a href="https://twitter.com/?lang=en" target="_blank">
-          <img src="https://cdn-icons-png.flaticon.com/128/2525/2525779.png" alt="ig" style={{ width: "50px" }} />
-        </a>
+          <a href="https://www.instagram.com/" target="_blank">
+            <img src="https://cdn-icons-png.flaticon.com/128/1400/1400487.png" alt="ig" style={{ width: "50px" }} />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank">
+            <img src="https://cdn-icons-png.flaticon.com/128/1400/1400477.png" alt="ig" style={{ width: "50px" }} />
+          </a>
+          <a href="https://web.whatsapp.com/">
+            <img src="https://cdn-icons-png.flaticon.com/128/3820/3820293.png" alt="ig" style={{ width: "50px" }} />
+          </a>
+          <a href="https://twitter.com/?lang=en" target="_blank">
+            <img src="https://cdn-icons-png.flaticon.com/128/2525/2525779.png" alt="ig" style={{ width: "50px" }} />
+          </a>
 
-      </div>
-      <div class="item4">
-        <div id='map'>
-          <MapBox />
+        </div>
+        <div class="item4">
+          <div id='map'>
+            <MapBox />
+          </div>
         </div>
       </div>
     </div>
