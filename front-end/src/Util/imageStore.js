87b8f153aa0d43a55ae6c266868b0909779d4fa2
Daniel Naranjo <75263240+PompaDonpa@users.noreply.github.com>
2021-10-01 16:16:17 -0400

    Update imageStore.js

--- front-end/src/Util/imageStore.js
+++ front-end/src/Util/imageStore.js
@@ -1,3 +1,27 @@
-export const firestoreThisImage = async (image) => {
-    return {}
+import firebase from 'firebase'
+import { app } from '../firebase'
+
+const db = app.firestore()
+const storage = app.storage()
+
+const storeProfilePictureOnFirebase = async (file, email) => {
+  const storageRef = storage.ref()
+  const fileRef = storageRef.child(file.name)
+  await fileRef.put(file)
+
+  return db
+    .collection('UsersProfiles')
+    .doc(email)
+    .update({
+      images: firebase.firestore.FieldValue.arrayUnion({
+        name: file.name,
+        url: await fileRef.getDownloadURL(),
+        album: email
+      })
+    })
+}
+
+export const firestoreThisImage = async (file, email, userAlbum) => {
+  file && (await storeProfilePictureOnFirebase(file, email))
+  return true
 }
