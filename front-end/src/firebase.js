9fbe275a81408fbe0e7bfeded755a604d31150ea
Daniel Naranjo <75263240+PompaDonpa@users.noreply.github.com>
2021-10-01 16:33:34 -0400

    Update firebase.js

--- front-end/src/firebase.js
+++ front-end/src/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-const app = firebase.initializeApp({
+export const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -15,4 +15,4 @@ const app = firebase.initializeApp({
 })
 
 export const auth = app.auth()
-export default app
+
