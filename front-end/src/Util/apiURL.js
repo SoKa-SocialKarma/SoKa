e6ce9c07d227217e2bb707ddee1cc846e8523b7b
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-10-05 13:54:38 -0400

    Map Fix attempt

--- front-end/src/Util/apiURL.js
+++ front-end/src/Util/apiURL.js
@@ -1,5 +1,11 @@
 export const apiURL = () => {
-    return window.location.hostname === "localhost"
-      ? "http://localhost:4000"
-      : "";
-  };
+  return window.location.hostname === 'localhost'
+    ? 'http://localhost:4000'
+    : 'https://social-karma.herokuapp.com'
+}
+
+export const sokaURL = () => {
+  return window.location.hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : 'https://social-karma.netlify.app'
+}
